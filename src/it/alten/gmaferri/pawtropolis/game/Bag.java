9a5eec46f7b5fac684a53cae3229ff624bd82fba
Metaliller <gm200110@gmail.com>
2023-12-04 11:55:35 +0100

    Implement Base Method

--- src/it/alten/gmaferri/pawtropolis/game/Bag.java
+++ src/it/alten/gmaferri/pawtropolis/game/Bag.java
@@ -2,13 +2,47 @@ package it.alten.gmaferri.pawtropolis.game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Bag {
-    private List<Item> items;
-
-    private final int SLOT = 10;
-
+    private final List<Item> items;
+    private final int slot;
     public Bag(){
+        slot = 10;
         items = new ArrayList<>();
     }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public int getTotalWeight(){
+        return items.stream()
+                .mapToInt(Item::getWeight)
+                .sum();
+    }
+
+    public boolean addItem(Item item){
+        if(getTotalWeight() + item.getWeight() < slot ){
+            return items.add(item);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean removeItem(Item item){
+       return items.remove(item);
+    }
+
+    public String showItems(){
+        return items.stream()
+                .map(Item::toString)
+                .collect(Collectors.joining(""));
+    }
+
 }
