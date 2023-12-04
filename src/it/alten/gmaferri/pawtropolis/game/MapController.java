d44d31694ff2d5987aa5ee26983ce7396850972c
Metaliller <gm200110@gmail.com>
2023-12-04 12:53:01 +0100

    Create Map

--- src/it/alten/gmaferri/pawtropolis/game/MapController.java
+++ src/it/alten/gmaferri/pawtropolis/game/MapController.java
@@ -1,18 +1,31 @@
 package it.alten.gmaferri.pawtropolis.game;
 
+import it.alten.gmaferri.pawtropolis.game.factory.RoomFactory;
+import it.alten.gmaferri.pawtropolis.game.model.Room;
+
 public class MapController {
     private Room[][] rooms;
 
     public MapController(){
-
+        rooms = createMap();
     }
 
-    public static Room[][] createBoard(){
-        RoomFactory instance = RoomFactory.getInstance();
-        return new Room[][] { {instance.createRoom()},
-                {instance.createRoom(), instance.createRoom(), instance.createRoom()},
-                { },
-                { }};
+    public Room[][] getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Room[][] rooms) {
+        this.rooms = rooms;
+    }
+
+    public static Room[][] createMap(){
+        RoomFactory roomFactory = RoomFactory.getInstance();
+        return new Room[][] {
+                {roomFactory.createRoom(), null, null, null},
+                {roomFactory.createRoom(), roomFactory.createRoom(), roomFactory.createRoom(), null},
+                {roomFactory.createRoom(), roomFactory.createRoom(), null, roomFactory.createRoom() },
+                {roomFactory.createRoom(), roomFactory.createRoom(), null, roomFactory.createRoom()}
+        };
 
     }
 
