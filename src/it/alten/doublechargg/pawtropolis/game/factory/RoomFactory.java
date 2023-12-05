48e280c26e323e380381c2380228d78e68fff0e4
Metaliller <gm200110@gmail.com>
2023-12-05 15:37:02 +0100

    Implementation RoomFactory

--- src/it/alten/doublechargg/pawtropolis/game/factory/RoomFactory.java
+++ src/it/alten/doublechargg/pawtropolis/game/factory/RoomFactory.java
@@ -1,5 +1,6 @@
 package it.alten.doublechargg.pawtropolis.game.factory;
 
+import it.alten.doublechargg.pawtropolis.game.model.Item;
 import it.alten.doublechargg.pawtropolis.game.model.Room;
 
 import java.util.Objects;
@@ -7,8 +8,15 @@ import java.util.Random;
 
 public class RoomFactory {
     private static RoomFactory instance = null;
+    static int idRoom = 0;
 
     Random randomizer = new Random();
+
+    Item[] items = {
+            new Item(1L,"Sword", "A sword", 7),
+            new Item(2L,"Knife", "A knife", 4),
+            new Item(3L,"Torch", "A Torch", 2)
+    };
 
     private RoomFactory(){
 
@@ -22,6 +30,11 @@ public class RoomFactory {
     }
 
     public Room createRoom(){
-        return new Room(Integer.toHexString(randomizer.nextInt()));
+        idRoom++;
+        Room room =  new Room(Integer.toString(idRoom));
+        while (room.getItems().size() <= 4){
+            room.getItems().add(items[randomizer.nextInt(items.length)]);
+        }
+        return room;
     }
 }
