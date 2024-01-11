44162f5f30116249284f3994371aa433b813e3fa
Metaliller <gm200110@gmail.com>
2024-01-11 19:10:27 +0100

    FEAT//CreatedProject

---
+++ src/test/java/it/alten/pawtropolis/PawtropolisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alten.pawtropolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawtropolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
