a42938db10e2e139f9a2d61d7e3c414f7f8c6020
Metaliller <gm200110@gmail.com>
2024-03-27 18:13:36 +0100

    Fixes&&Refactor

--- storage/postgres.sql
+++ storage/postgres.sql
@@ -6,28 +6,56 @@ CREATE TABLE Bag (
 CREATE TABLE Player (
     id SERIAL PRIMARY KEY,
     bag_id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR NOT NULL,
     health_points INT NOT NULL,
     FOREIGN KEY (bag_id) REFERENCES Bag(id)
 );
 
-CREATE TABLE Room (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
 
 CREATE TABLE Room (
     id SERIAL PRIMARY KEY,
-    direction VARCHAR(255) NOT NULL
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE Direction(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+)
+
+INSERT INTO Direction (name) VALUES ('North'), ('South'), ('West'), ('East');
+
+CREATE TABLE Door (
+    id SERIAL PRIMARY KEY,
+    direction_id_id INT NOT NULL,
+    FOREIGN KEY (direction_id) REFERENCES Direction(id)
 );
 
 CREATE TABLE Item (
     id SERIAL PRIMARY KEY,
-    bag_id INT,
-    room_id INT,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    bag_id INT REFERENCES Bag(id),
+    room_id INT REFERENCES Room(id),
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
     weight int NOT NULL,
-    FOREIGN KEY (bag_id) REFERENCES Bag(id),
-    FOREIGN KEY (room_id) REFERENCES Room(id)
+);
+
+CREATE TABLE Species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
+
+INSERT INTO species (name) VALUES ('Lion'), ('Tiger'), ('Eagle');
+
+
+CREATE TABLE Animal (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    age INT,
+    arrival_date DATE,
+    weight DOUBLE PRECISION,
+    height DOUBLE PRECISION,
+    room_id INT REFERENCES room(id),
+    species_id INT REFERENCES species(id),
+    wingspan DOUBLE PRECISION,
+    tail_length DOUBLE PRECISION 
 );
